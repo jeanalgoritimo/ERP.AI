4d551b94d070154a0cd16ecd25ca8801a14190f8
jeanalgoritimo <jeanalgoritimo@gmail.com>
2026-08-14 08:50:12 -0300

    Feat:Melhorias

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.AI.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/FiscalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.AI.Controllers
+{
+    public class FiscalController : Controller
+    {
+        public IActionResult NotaFiscal()
+        {
+            return View();
+        }
+    }
+}
